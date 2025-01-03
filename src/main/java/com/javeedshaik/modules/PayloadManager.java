5d72ed105ef8ac1f29b19d745de6697dfb2522b1
awesomejaveed <javeedshaik1408@gmail.com>
2025-01-04 03:11:37 +0530

    ADD ALL

---
+++ src/main/java/com/javeedshaik/modules/PayloadManager.java
@@ -0,0 +1,4 @@
+package com.javeedshaik.modules;
+
+public class PayloadManager {
+}
---
+++ src/main/java/com/javeedshaik/pojos/TokenResponse.java
@@ -0,0 +1,12 @@
+package com.javeedshaik.pojos;
+
+public class TokenResponse {
+
+
+    private  String token;
+
+    public String getToken() {return token; }
+
+    public void setToken(String token) {this.token = token; }
+
+}
